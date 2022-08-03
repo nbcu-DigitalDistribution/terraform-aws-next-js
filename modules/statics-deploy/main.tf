027433477c385ba6c77406eb76c63eb2768615c1
Shyam Vala <shyam.vala@gmail.com>
2022-08-03 11:34:49 +0530

    debugging echos

--- modules/statics-deploy/main.tf
+++ modules/statics-deploy/main.tf
@@ -260,9 +260,6 @@ resource "null_resource" "static_s3_upload_awscli" {
     command = <<-EOT
       export AWS_ROLE_TO_ASSUME=${var.role_to_assume}
       ./get-creds
-      echo $AWS_ACCESS_KEY_ID
-      echo $AWS_SECRET_ACCESS_KEY
-      echo $AWS_SESSION_TOKEN
       aws s3 cp --region ${aws_s3_bucket.static_upload.region} ${abspath(var.static_files_archive)} s3://${aws_s3_bucket.static_upload.id}/${basename(var.static_files_archive)}
     EOT
     working_dir = "${path.module}/s3-bash4/bin"
@@ -281,7 +278,14 @@ resource "null_resource" "static_s3_upload" {
   }
 
   provisioner "local-exec" {
-    command     = "./s3-put -r ${aws_s3_bucket.static_upload.region} -T ${abspath(var.static_files_archive)} /${aws_s3_bucket.static_upload.id}/${basename(var.static_files_archive)}"
+    command = <<-EOT
+      export AWS_ROLE_TO_ASSUME=${var.role_to_assume}
+      ./get-creds
+      echo "CREDS"
+      echo $AWS_ACCESS_KEY_ID
+      echo $AWS_SECURITY_TOKEN
+      ./s3-put -r ${aws_s3_bucket.static_upload.region} -T ${abspath(var.static_files_archive)} /${aws_s3_bucket.static_upload.id}/${basename(var.static_files_archive)}
+    EOT
     working_dir = "${path.module}/s3-bash4/bin"
   }
 
